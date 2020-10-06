c72ab30e1ba2aacde8a0e3cfe84105129e652cbe
KyleClose09 <kclose@ualberta.ca>
2020-10-05 20:52:33 -0600

    sending project to github

---
+++ Assets/scripts/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private Renderer rend;
+    private Material mat;
+    public Color[] colors;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        mat = rend.material;
+
+    }
+    private void OnMouseDown()
+    {
+        mat.SetColor("_Color", Color.cyan);
+    }
+}
